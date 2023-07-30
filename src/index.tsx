a1deca24cfa5377c9ba3a745154fc4f49a16d430
Filippo Gianessi <gianessi.filippo@gmail.com>
2023-07-30 20:07:39 +0200

    edits

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,16 @@
-import React from "react";
+import React from 'react';
+// import ReactDOM from 'react-dom/client'; <- This import is only for React version 18
 import ReactDOM from "react-dom/client";
-import "./index.css";
+import './index.css';
 import "./tokens/css/design-tokens.css";
-import App from "./App/App";
+import App from './App/App';
+import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
-root.render(
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = document.getElementById('root'); // <- This is the correct method call for React version 17
+render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  root
 );
